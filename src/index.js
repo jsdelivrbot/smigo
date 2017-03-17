bdfc49e045a04b7a2fb79ecbb902016c95c1ee22
Sami Luukkonen <sami.m.luukkonen@gmail.com>
2017-03-17 11:07:11 +0200

    fix store to work with redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 
 import App from './components/app'
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers, /* preloadedState, */
+  composeEnhancers(
+    applyMiddleware()
+    // applyMiddleware(...middleware)
+  )
+)
 
 ReactDOM.render(
   (
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
       <App />
     </Provider>
   )
