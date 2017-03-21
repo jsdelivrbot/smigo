e603726ad0c8c9c1a5f8f21168988426ec151cc3
Sami Luukkonen <sami.m.luukkonen@gmail.com>
2017-03-21 16:20:33 +0200

    more functions

--- src/utils/scoreEstimator.js
+++ src/utils/scoreEstimator.js
@@ -3,9 +3,9 @@ const convnetjs = require('convnetjs')
 const learn = (trainer, net, data, labels) => {
   for(let j = 0; j < 2000; j++){
     labels.map((label, i) => {
-        const x = new convnetjs.Vol(data[i])
+      const x = new convnetjs.Vol(data[i])
 
-        trainer.train(x, [label])
+      trainer.train(x, [label])
     })
   }
 }
@@ -16,11 +16,11 @@ const predict = (net, data) => {
   return predicted_value.w[0]
 }
 
-const scoreEstimator = (board) => {
-  const { data, labels } = board.reduce((accumulator, current, indY) => {
-    const { coordinates, players } = current.reduce((accumulator, player, indX) => {
+const getDataAndLabels = (board) => {
+  return board.reduce((accumulator, current, y) => {
+    const { coordinates, players } = current.reduce((accumulator, player, x) => {
       if (player) {
-        accumulator.coordinates.push([indX, indY])
+        accumulator.coordinates.push([x, y])
         accumulator.players.push(player)
       }
 
@@ -34,12 +34,10 @@ const scoreEstimator = (board) => {
 
     return accumulator
   }, { data: [], labels: [] })
+}
 
-  if (data.length === 0) {
-    return false
-  }
-
-  var net = new convnetjs.Net()
+const createNet = () => {
+  const net = new convnetjs.Net()
 
   net.makeLayers([
     { type: 'input', out_sx: 1, out_sy: 1, out_depth: 2 },
@@ -48,12 +46,28 @@ const scoreEstimator = (board) => {
     { type: 'regression', num_neurons: 1 }
   ])
 
-  const trainer = new convnetjs.SGDTrainer(net, {
+  return net
+}
+
+const createTrainer = (net) => {
+  return new convnetjs.SGDTrainer(net, {
     learning_rate: 0.01,
     momentum: 0.2,
     batch_size: 1,
     l2_decay: 0.001
   })
+}
+
+const scoreEstimator = (board) => {
+  const { data, labels } = getDataAndLabels(board)
+
+  if (data.length === 0) {
+    return false
+  }
+
+  const net = createNet()
+
+  const trainer = createTrainer(net)
 
   learn(trainer, net, data, labels)
 
