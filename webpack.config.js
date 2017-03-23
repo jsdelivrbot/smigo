569da8924c97efde4093fd3737b09168438a5d4c
Sami Luukkonen <sami.m.luukkonen@gmail.com>
2017-03-23 16:26:19 +0200

    use manifest chunk

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,12 @@ module.exports = {
           // this assumes your vendor imports exist in the node_modules directory
           return module.context && module.context.indexOf('node_modules') !== -1;
       }
+    }),
+    // CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+    new webpack.optimize.CommonsChunkPlugin({
+      // But since there are no more common modules between them
+      // we end up with just the runtime code included in the manifest file
+      name: 'manifest'
     })
   ],
   module: {
