8ccef9d2b961486e7d525bf11911ab9a824f5764
Sami Luukkonen <sami.m.luukkonen@gmail.com>
2017-03-24 10:40:42 +0200

    developtment & production environments

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+"use strict"
+
 const webpack = require('webpack')
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const debug = process.env.NODE_ENV !== "production"
 
 module.exports = {
   entry: {
@@ -9,32 +13,44 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: '[name].[chunkhash].js',
+    filename: debug ? 'bundle.js' : '[name].[chunkhash].js',
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module) {
-          // this assumes your vendor imports exist in the node_modules directory
-          return module.context && module.context.indexOf('node_modules') !== -1;
-      }
-    }),
-    // CommonChunksPlugin will now extract all the common modules from vendor and main bundles
-    new webpack.optimize.CommonsChunkPlugin({
-      // But since there are no more common modules between them
-      // we end up with just the runtime code included in the manifest file
-      name: 'manifest'
-    }),
-    new CleanWebpackPlugin(['dist'], {
-      root: __dirname,
-      verbose: true,
-      dry: false,
-      exclude: ['shared.js']
-    }),
-    new HtmlWebpackPlugin({
-      title: 'smigo: A Go application'
-    })
-  ],
+  plugins: debug
+    ? [
+        new HtmlWebpackPlugin({
+          title: 'smigo: A Go application',
+          template: 'index.html',
+          filename: path.resolve(__dirname, 'index.html'),
+          inject: 'body'
+        })
+      ]
+    : [
+        new CleanWebpackPlugin(['dist'], {
+          root: __dirname,
+          verbose: true,
+          dry: false,
+          exclude: ['shared.js']
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+          name: 'vendor',
+          minChunks: function (module) {
+              // this assumes your vendor imports exist in the node_modules directory
+              return module.context && module.context.indexOf('node_modules') !== -1;
+          }
+        }),
+        // CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+        new webpack.optimize.CommonsChunkPlugin({
+          // But since there are no more common modules between them
+          // we end up with just the runtime code included in the manifest file
+          name: 'manifest'
+        }),
+        new HtmlWebpackPlugin({
+          title: 'smigo: A Go application',
+          template: 'index_dist.html',
+          filename: path.resolve(__dirname, 'dist/index.html'),
+          inject: 'body'
+        }),
+      ],
   module: {
     rules: [
       {
