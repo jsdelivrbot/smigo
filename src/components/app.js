239f0b29408316b6dfb099340bb6b56148a6f662
Sami Luukkonen <sami.m.luukkonen@gmail.com>
2017-03-27 16:57:44 +0300

    use react router

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-
+import { Route, Link } from 'react-router-dom'
 import { Layout, Menu } from 'antd'
+
 const { Header, Content, Footer } = Layout
 
-import Game from './Game'
+import Home from './home'
+import Game from './game'
 
 class App extends Component {
   render() {
@@ -17,14 +19,15 @@ class App extends Component {
             defaultSelectedKeys={['1']}
             style={{ lineHeight: '64px' }}
           >
-            <Menu.Item key="1">Home</Menu.Item>
-            <Menu.Item key="2">Game</Menu.Item>
+            <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
+            <Menu.Item key="2"><Link to="game">Game</Link></Menu.Item>
             <Menu.Item key="3">Editor</Menu.Item>
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
           <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-            <Game />
+            <Route exact path="/" component={Home} />
+            <Route path="/game" component={Game} />
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
