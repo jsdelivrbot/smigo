cee7820a2044f20e91c59ce6aa0d125cf6c4b3b3
Sami Luukkonen <sami.m.luukkonen@gmail.com>
2017-03-28 16:48:14 +0300

    codestyle

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 8081
 const app = express()
-const formidable = require('formidable')
-const fs = require('fs')
 
 const bodyParser = require('body-parser')
 
@@ -14,6 +12,7 @@ const cors = require('cors')
 app.use(cors())
 
 const scoreEstimator = require('./src/utils/scoreEstimator').scoreEstimator
+const handleUpload = require('./src/utils/handleUpload').handleUpload
 const SGFParser = require('./src/utils/SGF_parser').SGFParser
 
 app.use(express.static(__dirname))
@@ -36,39 +35,10 @@ app.post('/api/predict', (req, res) => {
 })
 
 app.post('/api/upload', (req, res) => {
-  // create an incoming form object
-  const form = new formidable.IncomingForm()
-  let filePath = ""
-
-  // specify that we want to allow the user to upload multiple files in a single request
-  form.multiples = false
-
-  // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, '/uploads')
-
-  // every time a file has been uploaded successfully,
-  // rename it to it's orignal name
-  form.on('file', function(field, file) {
-    filePath = path.join(form.uploadDir, file.name)
-
-    fs.renameSync(file.path, filePath)
-  })
-
-  // log any errors that occur
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err)
-  })
-
-  // once all the files have been uploaded, send a response to the client
-  form.on('end', function() {
+  handleUpload(req, res, (filePath, response) => {
     SGFParser(filePath)
-    .then(response => {
-      res.send(JSON.stringify(response))
-    })
+      .then(parsedGame => { response.send(JSON.stringify(parsedGame)) })
   })
-
-  // parse the incoming request containing the form data
-  form.parse(req)
 })
 
 app.get('*', (req, res) => {
