eb986867154b1b64a2c7f25460917b1a6fca37c5
Sami Luukkonen <sami.m.luukkonen@gmail.com>
2017-03-31 14:23:43 +0300

    fast forwarding & fast backwarding

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { message, Card, Row, Col, Icon } from 'antd'
+import { message, Card, Row, Col, Icon, Button } from 'antd'
 
 import Uploader from './uploader'
 import Board from './board/board'
@@ -9,18 +9,33 @@ class Editor extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { match: null }
+    this.state = {
+      match: null,
+      index: null,
+      board: null,
+    }
 
     this.handleUploaderOnChange = this.handleUploaderOnChange.bind(this)
     this.renderInfo = this.renderInfo.bind(this)
     this.renderBoard = this.renderBoard.bind(this)
+    this.handleForward = this.handleForward.bind(this)
+    this.handleFastForward = this.handleFastForward.bind(this)
+    this.handleBackward = this.handleBackward.bind(this)
+    this.handleFastBackward = this.handleFastBackward.bind(this)
+    this.parseMove = this.parseMove.bind(this)
+    this.updateBoard = this.updateBoard.bind(this)
+    this.renderFastForward = this.renderFastForward.bind(this)
   }
 
   handleUploaderOnChange(info) {
     const { status, response: match, name } = info.file
 
     if (status === 'done') {
-      this.setState({ match }, message.success(`${name} file uploaded successfully.`))
+      // typecast size to integer
+      const size = Number(match.size)
+      const board = generateBoard(size)
+
+      this.setState({ match, board }, message.success(`${name} file uploaded successfully.`))
     }
     else if (status === 'error') {
       message.error(`${name} file upload failed.`)
@@ -76,10 +91,7 @@ class Editor extends Component {
     )
   }
 
-  renderBoard() {
-    // typecast size to integer
-    const size = Number(this.state.match.size)
-
+  renderBoard(state) {
     return (
       <div style={{ display: "inline-block", marginTop: "40px", width: "100%" }}>
         <div style={{ display: "inline-block", marginLeft: "-60px" }}>
@@ -87,37 +99,155 @@ class Editor extends Component {
             type={"editor"}
             onClick={() => {}}
             onCheckTurn={() => {}}
-            board={generateBoard(size)}
+            board={state.board}
           />
         </div>
-        {this.renderControls()}
+        {this.renderControls(state)}
       </div>
     )
   }
 
-  renderControls() {
-    const style = { fontSize: "20px", margin: "10px" }
+  renderControls(state) {
+    const style = { margin: "10px" }
+
+    const disabled = {
+      backwards: state.index === null ? true : false,
+      forwards: ((state.index + 1) === state.match.moves.length) ? true : false,
+    }
 
     return (
       <Row style={{ marginTop: "20px" }}>
         <Col span={12} offset={6}>
-          <Icon type="fast-backward" style={style} />
-          <Icon type="caret-left" style={style} />
-          <Icon type="caret-right" style={style} />
-          <Icon type="fast-forward" style={style} />
+          <Button
+            shape="circle"
+            icon="fast-backward"
+            size="large"
+            style={style}
+            disabled={disabled.backwards}
+            onClick={this.handleFastBackward}
+          />
+          <Button
+            shape="circle"
+            icon="caret-left"
+            size="large"
+            style={style}
+            disabled={disabled.backwards}
+            onClick={this.handleBackward}
+          />
+          <Button
+            shape="circle"
+            icon="caret-right"
+            size="large"
+            style={style}
+            disabled={disabled.forwards}
+            onClick={this.handleForward}
+          />
+          <Button
+            shape="circle"
+            icon="fast-forward"
+            size="large"
+            style={style}
+            disabled={disabled.forwards}
+            onClick={this.handleFastForward}
+          />
         </Col>
       </Row>
     )
   }
 
+  handleForward(event, state = null) {
+    state = !state ? this.state : state
+
+    const { match, index } = state
+    let nextIndex = index === null ? 0 : index + 1
+
+    if (nextIndex === match.moves.length) {
+      this.renderControls(state)
+      return
+    }
+
+    return this.updateBoard(state, nextIndex)
+  }
+
+  handleFastBackward() {
+    const { size } = this.state.match
+    const board = generateBoard(Number(size))
+    this.setState({ board, index: null })
+  }
+
+  handleFastForward() {
+    this.renderFastForward(this.state, this.handleForward)
+  }
+
+  renderFastForward(state, callback) {
+    const moves = state.match.moves
+
+    moves.map((val, index) => {
+      const nextIndex = callback(null, state)
+
+      state.index = nextIndex
+    })
+  }
+
+  handleBackward() {
+    const { index } = this.state
+    let nextIndex = index < 0 ? null : index - 1
+
+    this.updateBoard(this.state, nextIndex)
+  }
+
+  parseMove(state, index) {
+    const { match } = state
+
+    const move = match.moves[index].slice(2, 4)
+    const player = match.moves[index].slice(0, 1) === "B" ? 1 : 2
+
+    const [x, y] = move.split("")
+
+    return [player, x.charCodeAt(0) - 97, y.charCodeAt(0) - 97]
+  }
+
+  updateBoard(state, index) {
+    let board = state.board
+
+    if (state.index === null || index > state.index) {
+      const [player, x, y] = this.parseMove(state, index)
+
+      board = this.addMove({ player, x, y, board })
+    }
+
+    if (index === null || index < state.index) {
+      index = index === null ? 0 : index + 1
+
+      const [player, x, y] = this.parseMove(state, index)
+
+      board = this.removeMove({ x, y, board })
+      index = index === 0 ? null : index - 1
+    }
+
+    this.setState({ board, index })
+    return index
+  }
+
+  addMove({ player, x, y, board }) {
+    board[y][x] = player
+
+    return board
+  }
+
+  removeMove({ x, y, board }) {
+    board[y][x] = 0
+
+    return board
+  }
+
   render() {
-    console.log(this.state.match)
     if (!this.state.match) return <Uploader onChange={this.handleUploaderOnChange} />
 
     return (
       <div style={{ textAlign: "center" }}>
         {this.renderInfo()}
-        {this.renderBoard()}
+        {this.renderBoard(this.state)}
       </div>
     )
   }
