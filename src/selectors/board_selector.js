06439445f826b0ba1a7db2bef739e658f3163c77
Sami Luukkonen <sami.m.luukkonen@gmail.com>
2017-04-07 10:25:52 +0300

    create deep equal selector

--- src/selectors/board_selector.js
+++ src/selectors/board_selector.js
@@ -1,8 +1,15 @@
-import { createSelector } from 'reselect'
+import { createSelectorCreator, defaultMemoize } from 'reselect'
+import isEqual from 'lodash/isEqual'
+
+// create a "selector creator" that uses lodash.isEqual instead of ===
+const createDeepEqualSelector = createSelectorCreator(
+  defaultMemoize,
+  isEqual
+)
 
 export const getBoard = state => state.board
 
-export const getBoardLayout = createSelector(
+export const getBoardLayout = createDeepEqualSelector(
   [getBoard],
   (board) => {
     console.log('getBoardLayout called', board)
