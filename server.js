33224d1de466c5e8578eb36b74a084f2305d9efd
Sami Luukkonen <sami.m.luukkonen@gmail.com>
2017-04-10 19:50:12 +0300

    socket.io chat to homepage

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const app = express()
+
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server)
+
 const path = require('path')
 const port = process.env.PORT || 8081
-const app = express()
 
 // use body-parser middleware
 const bodyParser = require('body-parser')
@@ -25,6 +30,16 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
-app.listen(port)
+io.on('connection', (socket) => {
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  })
+
+  socket.on('disconnect', () => {
+    // console.log('user disconnected')
+  })
+})
+
+server.listen(port)
 
 console.log(`Server started. Listening to port ${port}.`)
--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,119 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-const Home = () => <div>Landing page...</div>
+import { getUserInfo } from '../selectors/login_selector'
 
-export default Home
+import { Row, Col, Input, Button, Card, Form } from 'antd'
+const FormItem = Form.Item
+
+// socket.io
+import io from 'socket.io-client'
+const url = `http://${window.location.hostname}:8081`
+const socket = io(url)
+
+socket.on('error', console.error.bind(console))
+
+socket.on('connect', () => {
+  // console.log('connected to server')
+})
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      messages: [],
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+    socket.on('chat message', (msg) => {
+      const { messages } = this.state
+
+      this.setState({
+        messages: [
+          ...messages,
+          msg
+        ]
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    socket.removeListener('chat message')
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        const { message } = values
+
+        const user = this.props.user
+
+        socket.emit('chat message', { user, message })
+
+        this.props.form.setFieldsValue({ message: "" })
+      }
+    })
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form
+
+    return (
+      <div>
+        <Row>
+          <Col span={24}>
+            <Card style={{ height: "600px", marginBottom: "20px" }}>
+              {this.state.messages.map((obj, i) => {
+                return (
+                  <Row key={`msg-${i}`}>
+                    <Col span={4}>
+                      {obj.user === null ? "Anonymous" : obj.user.name}:
+                    </Col>
+                    <Col span={20}>
+                      {obj.message}
+                    </Col>
+                    <hr />
+                  </Row>
+                )
+              })}
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Col span={23}>
+              <FormItem>
+                {getFieldDecorator('message', {
+                  rules: [{ required: true, message: 'You need to write something' }],
+                })(
+                  <Input placeholder="Write some text..." />
+                )}
+              </FormItem>
+            </Col>
+            <Col span={1}>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Send
+              </Button>
+            </Col>
+          </Form>
+        </Row>
+      </div>
+    )
+  }
+}
+
+const HomeForm = Form.create()(Home)
+
+function mapStateToProps(state) {
+  return {
+    user: getUserInfo(state),
+  }
+}
+
+export default connect(mapStateToProps)(HomeForm)
